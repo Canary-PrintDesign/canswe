08927613e7f04191ffd1405c9591c6af2d3c6cee
Gregory Daynes <gregdaynes@gmail.com>
2018-05-13 13:13:31 -0700

    Add geojson script
    Takes a csv with the headers
    
    `dealer_name`
    `city`
    `address`
    `provice`
    `postal_code`
    `phone`
    `longitude`
    `lattitude`
    
    and outputs a geojson compatible file
    
    usage:
    
    `node ./scripts/geojson /path/to/csv`

---
+++ scripts/geojson/index.js
@@ -0,0 +1,68 @@
+const inputFile = process.argv[2]
+const csv = require('csvtojson');
+const fs = require('fs');
+const crypto = require('crypto');
+
+class GeoJSON {
+  constructor(features) {
+    this.features = features;
+    this.type = "FeatureCollection"
+  }
+
+  get toJson() {
+    return {
+      "features": this.feature.toJson,
+      "type": this.type
+    }
+  }
+}
+
+class Feature {
+  constructor(data) {
+    this.type = "Feature";
+    this.properties = new Properties(data);
+    this.geometry = new Geometry(data);
+    this.id = crypto.createHash('md5').update(data.dealer_name).digest('hex')
+  }
+
+  get toJson() {
+    return {
+      type: this.type,
+      properties: this.properties,
+      geometry: this.geometry
+    }
+  }
+}
+
+class Properties {
+  constructor(data) {
+    this.dealer_name = data.dealer_name;
+    this.city = data.city;
+    this.address = data.address;
+    this.province = data.province;
+    this.postal_code = data.postal_code;
+    this.phone = data.phone;
+  }
+}
+
+class Geometry {
+  constructor(data) {
+    this.coordinates = [parseFloat(data.longitude), parseFloat(data.latitude)];
+    this.type = "Point"
+  }
+}
+
+let features = [];
+
+csv()
+  .fromFile(inputFile)
+  .on('json', (jsonObj) => {
+    features.push(new Feature(jsonObj));
+  })
+  .on('done', (error) => {
+    fs.writeFile('./geo-json.json', JSON.stringify(new GeoJSON(features)), (err) => {
+      if (err) return console.error(err);
+
+      console.info("Data saved", "./geo-json.json");
+    });
+  });
