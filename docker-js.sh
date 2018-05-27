58b4b4c65627630b2f73e5f13fbb7da12762977b
Gregory Daynes <gregdaynes@Evil.local>
2018-05-27 11:07:18 -0700

    Removing Docker
    At this point in time, there are too many issues with docker
    interacting with the macos filesystem to be productive.

--- docker-js.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-mkdir -p dist/assets/js
-
-npx babel src/ --out-file dist/assets/js/app.js --source-maps
--- docker-scss.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-mkdir -p dist/assets/css
-
-npx node-sass src/assets/css/main.scss --source-map-embed true -s | npx postcss --use autoprefixer -b 'last 1 versions' -o /opt/dist/assets/css/main.css
