4b64792f2716db9ff435f905ab3e38aa9631c860
Gregory Daynes <gregdaynes@Evil.local>
2018-05-29 20:52:09 -0700

    Strip out auto prefixer for now

--- dev.sh
+++ dev.sh
@@ -4,4 +4,6 @@ mkdir -p dist/assets/css
 
 trap 'echo "Bye!!!"; kill %1; kill %2; kill %3' SIGINT SIGKILL SIGQUIT
 
-bundle exec jekyll build --watch --config 'src/_config.yml,src/_config-common.yml,src/_config-development.yml' --source src/ --destination dist/ --incremental & npx babel src/ --out-file dist/assets/js/app.js --source-maps & npx node-sass src/assets/css/main.scss --source-map-embed true -s | npx postcss --use autoprefixer -b 'last 1 versions' -o dist/assets/css/main.css & npx browser-sync dist
+npx node-sass src/assets/css/main.scss --output dist/assets/css/main.css
+
+bundle exec jekyll build --watch --config 'src/_config.yml,src/_config-common.yml,src/_config-development.yml' --source src/ --destination dist/ --incremental & npx babel src/ --out-file dist/assets/js/app.js --source-maps & npx node-sass --watch --recursive src/assets/css/main.scss --source-map-embed true --output dist/assets/css/main.css & npx browser-sync dist
