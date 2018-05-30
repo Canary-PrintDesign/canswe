a9b3ff80bcf0e307c871b6320b0fa1a8df035d4a
Gregory Daynes <gregdaynes@Evil.local>
2018-05-29 21:58:34 -0700

    Remove existing css + js, then run

--- dev.sh
+++ dev.sh
@@ -2,8 +2,12 @@
 mkdir -p dist/assets/js
 mkdir -p dist/assets/css
 
+rm -rf dist/assets/js/*
+rm -rf dist/assets/css/*
+npx node-sass src/assets/css/main.scss --output dist/assets/css/
+
 trap 'echo "Bye!!!"; kill %1; kill %2; kill %3' SIGINT SIGKILL SIGQUIT
 
-npx node-sass src/assets/css/main.scss --output dist/assets/css/main.css
 
-bundle exec jekyll build --watch --config 'src/_config.yml,src/_config-common.yml,src/_config-development.yml' --source src/ --destination dist/ --incremental & npx babel src/ --out-file dist/assets/js/app.js --source-maps & npx node-sass --watch --recursive src/assets/css/main.scss --source-map-embed true --output dist/assets/css/main.css & npx browser-sync dist
+
+bundle exec jekyll build --watch --config 'src/_config.yml,src/_config-common.yml,src/_config-development.yml' --source src/ --destination dist/ --incremental & npx babel src/ --out-file dist/assets/js/app.js --source-maps & npx node-sass --watch --recursive src/assets/css/main.scss --source-map-embed true --output dist/assets/css/ & npx browser-sync dist
