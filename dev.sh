fd02d7f297a0171faf11f45cae7edd5708ff7e23
Gregory Daynes <gregdaynes@gmail.com>
2018-06-28 21:28:19 -0700

    Add JS to watchlist

--- dev.sh
+++ dev.sh
@@ -8,4 +8,4 @@ npx node-sass src/assets/css/main.scss --output dist/assets/css/
 
 trap 'echo "Bye!!!"; kill %1; kill %2; kill %3' SIGINT SIGKILL SIGQUIT
 
-bundle exec jekyll build --watch --config 'src/_config.yml,src/_config-common.yml,src/_config-development.yml' --source src/ --destination dist/ --incremental & npx babel src/ --out-file dist/assets/js/app.js --source-maps & npx node-sass --watch --recursive src/assets/css/main.scss --source-map-embed true --output dist/assets/css/ & npx browser-sync dist -w  -f ./dist
+bundle exec jekyll build --watch --config 'src/_config.yml,src/_config-common.yml,src/_config-development.yml' --source src/ --destination dist/ --incremental & npx babel src/ --watch --out-file dist/assets/js/app.js --source-maps & npx node-sass --watch --recursive src/assets/css/main.scss --source-map-embed true --output dist/assets/css/ & npx browser-sync dist -w  -f ./dist
